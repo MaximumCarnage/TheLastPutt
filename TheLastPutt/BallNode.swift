e00d375e54f2fdd36a73f764546b369c50c6ab48
Jaype27 <jaypee.0927@gmail.com>
2018-06-29 21:18:53 -0400

    display ball onto level, will need to tweak movement

--- TheLastPutt/BallNode.swift
+++ TheLastPutt/BallNode.swift
@@ -6,14 +6,12 @@
 //  Copyright © 2018 Dean,Dylan,JP,Mark. All rights reserved.
 //
 //
-import Foundation
 import SpriteKit
 
 
-class BallNode: SKScene {
+class BallNode: SKSpriteNode {
     
-    var ball = SKSpriteNode(imageNamed: "ball_generic1")
-    // TODO: placeholders at the moment
+
     var lastUpdateTime: TimeInterval = 0
     var dt: TimeInterval = 0
     
@@ -24,80 +22,82 @@ class BallNode: SKScene {
     var lastTouchLocation = CGPoint.zero
     var ballSelected: Bool = false
     
-    override func didMove(to view: SKView) {
-        // let ball = SKSpriteNode(imageNamed: "ball_generic1")
-        // ballNode = childNode(withName: "golf_ball") as! BallNode
-        
-        ball.position = CGPoint(x: 0, y: 0)
-        ball.setScale(2.0)
-        addChild(ball)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Use Init()")
     }
     
-    override func update(_ currentTime: TimeInterval) {
+     init() {
+        let texture = SKTexture(imageNamed: "ball_generic1")
+        super.init(texture: texture,color: .white,size: texture.size())
+        name = "Player"
+        zPosition = 50
+    }
+
+     func update(_ currentTime: TimeInterval) {
         if lastUpdateTime > 0 {
             dt = currentTime - lastUpdateTime
         } else {
             dt = 0
         }
         lastUpdateTime = currentTime
-        
-        move(sprite: ball, velocity: swipeVelocity)
+
+        move(sprite: texture, velocity: swipeVelocity)
     }
-    
-    func move(sprite: SKSpriteNode, velocity: CGPoint) {
+
+    func move(sprite: SKTexture?, velocity: CGPoint) {
         let amountToMove = CGPoint(x: velocity.x * CGFloat(dt), y: velocity.y * CGFloat(dt))
-        
-        sprite.position = CGPoint(x: sprite.position.x + amountToMove.x, y: sprite.position.y + amountToMove.y)
+
+        //TODO:Fix Movement = CGPoint(x: sprite.position.x + amountToMove.x, y: sprite.position.y + amountToMove.y)
     }
-    
-    // parameters may change depending on the outcome
-    func touchOffset(firstLocation: CGPoint, lastLocation: CGPoint) {
-        
-        // Theory: create a velocity between the two touch points,
-        // then apply that velocity then add to the ball position
-        // making them act parallel
-        let swipeVectorOffset = CGPoint(x: lastLocation.x - firstLocation.x, y: lastLocation.y - firstLocation.y)
-        let swipeLength = sqrt(Double(swipeVectorOffset.x * swipeVectorOffset.x + swipeVectorOffset.y * swipeVectorOffset.y))
-        let swipeDirection = (x: swipeVectorOffset.x / CGFloat(swipeLength), y: swipeVectorOffset.y / CGFloat(swipeLength))
-        
-        swipeVelocity = CGPoint(x: swipeDirection.x * ballSpeed, y: swipeDirection.y * ballSpeed)
-        
-        //        ball.position = CGPoint(x: ball.position.x + swipeVelocity.x, y: ball.position.y + swipeVelocity.y)
-    }
-    
-    //    func sceneTouched(firstTouch: CGPoint, lastTouch: CGPoint) {
-    //        touchOffset(firstLocation: firstTouch, lastLocation: lastTouch)
-    //    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // the ball has been selected
-        // find out where the finger has landed
-        
-        
-        //        if ballSelected {
-        //            firstTouchLocation = ball.position
-        //        }
-        
-        guard let touch = touches.first else {
-            return
-        }
-        firstTouchLocation = touch.location(in: self)
-        
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // y-value (uncertain) of arrow sprite will increase as the player moves touch away from ball
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // will launch ball in direction the finger was dragged to with velocity
-        guard let touch = touches.first else {
-            return
-        }
-        let touchLocation = touch.location(in: self)
-        lastTouchLocation = touchLocation
-        
-        
-        touchOffset(firstLocation: firstTouchLocation, lastLocation: lastTouchLocation)
-    }
+//
+//    // parameters may change depending on the outcome
+//    func touchOffset(firstLocation: CGPoint, lastLocation: CGPoint) {
+//
+//        // Theory: create a velocity between the two touch points,
+//        // then apply that velocity then add to the ball position
+//        // making them act parallel
+//        let swipeVectorOffset = CGPoint(x: lastLocation.x - firstLocation.x, y: lastLocation.y - firstLocation.y)
+//        let swipeLength = sqrt(Double(swipeVectorOffset.x * swipeVectorOffset.x + swipeVectorOffset.y * swipeVectorOffset.y))
+//        let swipeDirection = (x: swipeVectorOffset.x / CGFloat(swipeLength), y: swipeVectorOffset.y / CGFloat(swipeLength))
+//
+//        swipeVelocity = CGPoint(x: swipeDirection.x * ballSpeed, y: swipeDirection.y * ballSpeed)
+//
+//        //        ball.position = CGPoint(x: ball.position.x + swipeVelocity.x, y: ball.position.y + swipeVelocity.y)
+//    }
+//
+//    //    func sceneTouched(firstTouch: CGPoint, lastTouch: CGPoint) {
+//    //        touchOffset(firstLocation: firstTouch, lastLocation: lastTouch)
+//    //    }
+//
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        // the ball has been selected
+//        // find out where the finger has landed
+//
+//
+//        //        if ballSelected {
+//        //            firstTouchLocation = ball.position
+//        //        }
+//
+//        guard let touch = touches.first else {
+//            return
+//        }
+//        firstTouchLocation = touch.location(in: self)
+//        
+//    }
+//
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        // y-value (uncertain) of arrow sprite will increase as the player moves touch away from ball
+//    }
+//
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        // will launch ball in direction the finger was dragged to with velocity
+//        guard let touch = touches.first else {
+//            return
+//        }
+//        let touchLocation = touch.location(in: self)
+//        lastTouchLocation = touchLocation
+//
+//
+//        touchOffset(firstLocation: firstTouchLocation, lastLocation: lastTouchLocation)
+//    }
 }
--- TheLastPutt/GameScene.swift
+++ TheLastPutt/GameScene.swift
@@ -11,36 +11,57 @@ import GameplayKit
 import UIKit
 
 class GameScene: SKScene {
+    
+    // Ball Properties
+    var ball = BallNode()
+    var lastUpdateTime: TimeInterval = 0
+    var dt: TimeInterval = 0
+    var ballSpeed: CGFloat = 280.0
+    var velocity = CGPoint.zero
+    var swipeVelocity = CGPoint.zero
+    var firstTouchLocation = CGPoint.zero
+    var lastTouchLocation = CGPoint.zero
+    var ballSelected: Bool = false
+    
+    
+    
+    var background: SKTileMapNode!
     var obstaclesTileMap: SKTileMapNode?
     var currentLevel: Int = 0
-    
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+        
+    }
     
     override func didMove(to view: SKView) {
-        
-        
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+//        print("Running")
+//        // Get label node from scene and store it for use later
+//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+//        if let label = self.label {
+//            label.alpha = 0.0
+//            label.run(SKAction.fadeIn(withDuration: 2.0))
+//        }
+//
+//        // Create shape node to use during mouse interaction
+//        let w = (self.size.width + self.size.height) * 0.05
+//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+//
+//        if let spinnyNode = self.spinnyNode {
+//            spinnyNode.lineWidth = 2.5
+//
+//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+//                                              SKAction.fadeOut(withDuration: 0.5),
+//                                              SKAction.removeFromParent()]))
+//        }
+
+        //ball.position = CGPoint(x: 50, y: 50)
+        ball.setScale(2.0)
+        addChild(ball)
     }
     
     
@@ -51,7 +72,7 @@ class GameScene: SKScene {
             self.addChild(n)
         }
     }
-    
+
     func touchMoved(toPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
@@ -59,7 +80,7 @@ class GameScene: SKScene {
             self.addChild(n)
         }
     }
-    
+
     func touchUp(atPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
@@ -67,31 +88,49 @@ class GameScene: SKScene {
             self.addChild(n)
         }
     }
-    
-    
+
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let label = self.label {
             label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
         }
-        
+
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
+
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
-    
+
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+
+        guard let touch = touches.first else {
+            return
+        }
+        let touchLocation = touch.location(in: self)
+        lastTouchLocation = touchLocation
+        touchOffset(firstLocation: firstTouchLocation, lastLocation: lastTouchLocation)
     }
-    
+
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+
     }
-    
-    
+
+
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+        } else {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+
+        //move(sprite: ball, velocity: swipeVelocity)
+
+
     }
     func tile(in tileMap: SKTileMapNode,  at coordinates: TileCoordinates)
         -> SKTileDefinition? {
@@ -116,10 +155,34 @@ class GameScene: SKScene {
                 obstaclesTileMap.addChild(node)
             }
         }
-        
+    }
+
+    func move(sprite: SKSpriteNode, velocity: CGPoint) {
+        let amountToMove = CGPoint(x: velocity.x * CGFloat(dt), y: velocity.y * CGFloat(dt))
+
+        sprite.position = CGPoint(x: sprite.position.x + amountToMove.x, y: sprite.position.y + amountToMove.y)
+    }
+
+    // parameters may change depending on the outcome
+    func touchOffset(firstLocation: CGPoint, lastLocation: CGPoint) {
+
+        let swipeVectorOffset = CGPoint(x: lastLocation.x - firstLocation.x, y: lastLocation.y - firstLocation.y)
+        let swipeLength = sqrt(Double(swipeVectorOffset.x * swipeVectorOffset.x + swipeVectorOffset.y * swipeVectorOffset.y))
+        let swipeDirection = (x: swipeVectorOffset.x / CGFloat(swipeLength), y: swipeVectorOffset.y / CGFloat(swipeLength))
+
+        swipeVelocity = CGPoint(x: swipeDirection.x * ballSpeed, y: swipeDirection.y * ballSpeed)
+
+        // ball.position = CGPoint(x: ball.position.x + swipeVelocity.x, y: ball.position.y + swipeVelocity.y)
     }
     
-   
-    
+//    func golfBounds() {
+//        let leftRIghtBounds = CGPoint.zero
+//        let topBottomBounds = CGPoint(x: size.width, y: size.height)
+//
+//        if ball.position.x <= topBottomBounds.x {
+//            ball.position = topBottomBounds.x
+//            velocity.x = -velocity.x
+//        }
+//    }
     
 }
